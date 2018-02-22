16a68fef905adb055959c24f312574c5b3f95630
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-02-22 21:13:15 +0100

    Point class

---
+++ Ant Colony/point.py
@@ -0,0 +1,11 @@
+class Point:
+    x = 0
+    y = 0
+    pheromone_number = 0
+
+    def __init__(self,x_p,y_p):
+        self.x = x_p
+        self.y = y_p
+
+    def AddPheromone(self):
+        self.pheromone_number+=1
