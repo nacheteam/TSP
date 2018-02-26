5c662569c38e50933428cb50d30e6daada9247ff
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2018-02-26 15:48:44 +0100

    Added distance

--- Ant Colony/ant.py
+++ Ant Colony/ant.py
@@ -1,3 +1,8 @@
+import math
+
+def Distance(x,y):
+    return math.sqrt(abs(x.x-y.x)**2 + abs(x.y-y.y)**2)
+
 class Ant:
 
     #Index of the first point in the route
@@ -5,6 +10,3 @@ class Ant:
 
     def __init__(self,f_p):
         self.first_point = f_p
-
-    def
-        
